16b7694fbebc76f3fcf643832f45d34b28d4f0ab
Luhe_Xx07_ <79638088+Luke889@users.noreply.github.com>
2022-03-10 08:40:11 +0100

    Update main.java

--- IDTokenGenerator/src/IDTokenGen/main.java
+++ IDTokenGenerator/src/IDTokenGen/main.java
@@ -11,6 +11,7 @@ public class main {
         for(int i=0; i<12; i++){ //Die Max Zahl bei i=12 die 12 beliebig ändern
            
             zahl = 1+ id.nextInt(6);
+            System.out.println("Token:");
             System.out.print(zahl);
         }
 
